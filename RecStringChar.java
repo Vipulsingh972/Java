c2e721ed32cd4231822de710bd3ab90e47e9308a
Vipulsingh972 <vipulchouhan972@gmail.com>
2024-11-03 16:08:45 +0530

    Create RecStringChar.java
    Given str string move all character 'x' from end of the string by Recursion.

---
+++ RecStringChar.java
@@ -0,0 +1,32 @@
+class RecStringChar {
+    
+    public static void moveAllX(String str, int index, int count, String newString)
+    {
+        if(index == str.length())
+        {
+            for(int i=0; i<count; i++)
+            {
+                newString += 'x';
+            }
+            System.out.println(newString);
+            return;
+        }
+        
+        char currentChar = str.charAt(index);
+        if(currentChar == 'x')
+        {
+            count++;
+            moveAllX(str, index+1, count, newString);
+        }
+        else
+        {
+            newString += currentChar;
+            moveAllX(str, index+1, count, newString);
+        }
+    }
+    
+    public static void main(String[] args) {
+        String str = "abxcxxdef";
+        moveAllX(str, 0, 0, "");
+    }
+}
